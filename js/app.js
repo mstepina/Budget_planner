52ad6946bebe2dd2a3f4969e9e21bd833e706301
Margarita Stepina <margarita.stepina@gmail.com>
2020-01-16 21:52:46 -0800

    created eventListeners function

--- js/app.js
+++ js/app.js
@@ -16,3 +16,33 @@ class UI {
     this.itemID = 0;
   }
 }
+
+
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  //new instance of UI class
+  const ui = new UI();
+
+  //budget form submit
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault(); // prevent form automatically submitting
+  })
+
+  //expense form submit
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  })
+
+  //expense list click
+  expenseList.addEventListener('click', function(){
+    
+  })
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+})
